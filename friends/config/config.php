ad7e24713a368fd7be5ff83dfe367e51442a1c97
Markus Tacker <m@tacker.org>
2012-01-15 15:16:08 +0100

    Added path.

--- friends/config/config.php
+++ friends/config/config.php
@@ -4,7 +4,7 @@ if ($_SERVER['HTTP_HOST'] == 'friends.retext.it') {
     define('CONS_KEY', 'oWTWUJd6E1olMYDQlhTRjg');
     define('CONS_SECRET', 'oHOPRr4PX5Rip3CGBMk93dlB5cZkBVFM7uGINOROA');
     define('DEV', false);
-    define('DROPBOX', false);
+    define('DROPBOX', '/home/retext/Dropbox/retext/Dokumente/');
 } else {
     define('CONS_KEY', 'sCCto2dNIcydwhuumNEsVg');
     define('CONS_SECRET', 'wlcI9bqPYdOERi8j0hxZV8b73RZ0BRDRCbtyTvymQ');
