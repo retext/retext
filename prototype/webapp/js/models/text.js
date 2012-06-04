99708d9a79eca7db63c7a45732b33323f2ad85da
Markus Tacker <m@coderbyheart.de>
2012-06-04 19:39:54 +0200

    Buttons für Textbausteine dazu.

---
+++ prototype/webapp/js/models/text.js
@@ -0,0 +1,19 @@
+define([
+    'remote',
+    'models/base'
+], function (Remote, BaseModel) {
+    var Container = BaseModel.extend({
+        urlRoot:Remote.apiUrlBase + 'container',
+        defaults:{
+            '@relations':[],
+            id:null,
+            project:null,
+            name:null,
+            parent:null,
+            childcount:0
+        },
+        validate:function (attrs) {
+        }
+    });
+    return Container;
+});
