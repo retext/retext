ca6a484c9e6be8eef2dd6b9e008938e33d40af7d
Markus Tacker <m@coderbyheart.de>
2012-06-05 09:31:29 +0200

    Models nicht immer wieder automatisch anpassen.

--- prototype/server/bin/reset.sh
+++ prototype/server/bin/reset.sh
@@ -2,7 +2,6 @@
 
 ENV=$1
 
-app/console --env=$ENV doctrine:mongodb:generate:documents RetextApiBundle
 app/console --env=$ENV doctrine:mongodb:generate:hydrators
 app/console --env=$ENV doctrine:mongodb:generate:proxies
 app/console --env=$ENV doctrine:mongodb:generate:repositories RetextApiBundle
