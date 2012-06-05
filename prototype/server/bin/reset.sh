9847d2c6ede3a31fa421b3491fd0a13b0fc3bc4d
Markus Tacker <m@coderbyheart.de>
2012-06-05 15:52:19 +0200

    Fixed.

--- prototype/server/bin/reset.sh
+++ prototype/server/bin/reset.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 ENV=$1
+USER=$2
+if [[ $USER == "" ]];
+then
+    USER=`whoami`
+fi
 
 app/console --env=$ENV cache:clear
 app/console --env=$ENV doctrine:mongodb:generate:hydrators
@@ -8,3 +13,4 @@ app/console --env=$ENV doctrine:mongodb:generate:proxies
 app/console --env=$ENV doctrine:mongodb:generate:repositories RetextApiBundle
 app/console --env=$ENV doctrine:mongodb:schema:drop
 app/console --env=$ENV doctrine:mongodb:schema:create
+chown -R $USER: app/cache/$ENV
