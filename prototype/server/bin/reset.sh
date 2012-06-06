caec2523b8b1556f5b827a94c3e0c3eb318d71c1
Markus Tacker <m@tckr.cc>
2012-06-06 14:17:26 +0200

    cache::warm dazu.

--- prototype/server/bin/reset.sh
+++ prototype/server/bin/reset.sh
@@ -13,4 +13,5 @@ app/console --env=$ENV doctrine:mongodb:generate:proxies
 app/console --env=$ENV doctrine:mongodb:generate:repositories RetextApiBundle
 app/console --env=$ENV doctrine:mongodb:schema:drop
 app/console --env=$ENV doctrine:mongodb:schema:create
+app/console --env=$ENV cache:warm
 chown -R $USER: app/cache/$ENV
