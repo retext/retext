4bf7ef335d9e5c9658711b1fefd0be8ead094cf9
Markus Tacker <m@coderbyheart.de>
2012-06-16 12:34:12 +0200

    Anzahl im Projektfortschritt anzeigen.

--- prototype/webapp/js/views/modules/project/progress.js
+++ prototype/webapp/js/views/modules/project/progress.js
@@ -4,16 +4,16 @@ define([
 ], function (Model, ModuleTemplate) {
     return Backbone.View.extend({
         template:_.template(ModuleTemplate),
-        className: 'projectprogress',
+        className:'projectprogress',
         initialize:function () {
             this.model.bind("change", this.render, this);
         },
         render:function () {
             var data = [
-                {id: 'total', label:'Gesamt', percent:Math.max(0.01, this.model.get('total').progress) * 100},
-                {id: 'spelling', label:'Rechtschreibung', percent:Math.max(0.01, this.model.get('spellingApproved').progress) * 100},
-                {id: 'content', label:'Inhalt', percent:Math.max(0.01, this.model.get('contentApproved').progress) * 100},
-                {id: 'approval', label:'Freigabe', percent:Math.max(0.01, this.model.get('approved').progress) * 100}
+                {id:'total', label:'Gesamt', percent:Math.max(0.01, this.model.get('total').progress) * 100, completed:this.model.get('total').yes, total:this.model.get('total').yes + this.model.get('total').no},
+                {id:'spelling', label:'Rechtschreibung', percent:Math.max(0.01, this.model.get('spellingApproved').progress) * 100, completed:this.model.get('spellingApproved').yes, total:this.model.get('spellingApproved').yes + this.model.get('spellingApproved').no},
+                {id:'content', label:'Inhalt', percent:Math.max(0.01, this.model.get('contentApproved').progress) * 100, completed:this.model.get('contentApproved').yes, total:this.model.get('contentApproved').yes + this.model.get('contentApproved').no},
+                {id:'approval', label:'Freigabe', percent:Math.max(0.01, this.model.get('approved').progress) * 100, completed:this.model.get('approved').yes, total:this.model.get('approved').yes + this.model.get('approved').no}
             ];
             $(this.el).html(this.template({progresses:data}));
             return this;
