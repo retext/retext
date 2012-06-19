53984e26efe4e28e6f356fc437db54f7485e50e3
Markus Tacker <m@coderbyheart.de>
2012-06-19 17:50:34 +0200

    Fortschritt im Baum visualisieren.

--- prototype/webapp/js/views/modules/project/tree.js
+++ prototype/webapp/js/views/modules/project/tree.js
@@ -1,9 +1,12 @@
 define([
+    'events',
     'collections/treenode',
     'text!templates/modules/project/tree.html'
-], function (TreenodeCollection, ModuleTemplate) {
+], function (Events, TreenodeCollection, ModuleTemplate) {
     return Backbone.View.extend({
         className:'projecttree',
+        template:_.template(ModuleTemplate),
+        showTexts:false,
         events:{
             'click #tree-show-texts':'toggleTexts'
         },
@@ -12,14 +15,20 @@ define([
             this.tree = new TreenodeCollection();
             this.tree.url = this.model.get('container').getRelation('http://jsonld.retext.it/Element', true, 'http://jsonld.retext.it/ontology/tree').get('href');
             this.tree.bind("reset", this.render, this);
+            Events.on('projectProgressChanged', this.complete, this);
         },
         render:function () {
-            var tree = '<ul class="gui-tree hide-texts">';
+            var tree = '<ul class="gui-tree">';
+            if (!this.showTexts) {
+                tree = '<ul class="gui-tree hide-texts">';
+            } else {
+                tree = '<ul class="gui-tree">';
+            }
             _.each(this.tree.models, function (node) {
                 tree += this.renderNode(node.get('data'), node.get('children'));
             }, this);
             tree += '<ul>';
-            $(this.el).html(ModuleTemplate);
+            $(this.el).html(this.template({showTexts:this.showTexts}));
             $(this.el).find('.gui-tree').html(tree);
             return this;
         },
@@ -33,7 +42,15 @@ define([
             } else {
                 nodeHtml += '<li class="gui-text-node"><i class="icon-pencil"></i> ';
             }
-            nodeHtml += '<span class="name">' + data.name + '</span>';
+            nodeHtml += '<span class="name">' + (_.isUndefined(data.name) ? '<em>Kein Name</em>' : data.name) + '</span>';
+            if (data['@context'] == 'http://jsonld.retext.it/Text') {
+                if (data.approvedProgress < 0.5) {
+                    nodeHtml += ' <i class="icon-exclamation-sign" title="Weniger als die Hälfte erledigt"></i>';
+                } else if (data.approvedProgress < 1) {
+                    nodeHtml += ' <i class="icon-adjust" title="Mehr als die Hälfte erledigt"></i>';
+                }
+            }
+
             if (link) nodeHtml += '</a>';
             if (children.length > 0) {
                 nodeHtml += '<ul>';
@@ -51,8 +68,10 @@ define([
         toggleTexts:function (ev) {
             var input = $(ev.target).closest('input');
             if (_.isUndefined(input.attr('checked'))) {
+                this.showTexts = false;
                 $(this.el).find('ul.gui-tree').addClass('hide-texts');
             } else {
+                this.showTexts = true;
                 $(this.el).find('ul.gui-tree').removeClass('hide-texts');
             }
         }
