72ea87e1e3c5bcb3ba1901c61958e375ccddbe8a
Markus Tacker <m@tacker.org>
2012-06-20 15:59:42 +0200

    Prod-Version einrichten.

--- prototype/server/bin/reset.sh
+++ prototype/server/bin/reset.sh
@@ -15,3 +15,6 @@ app/console --env=$ENV doctrine:mongodb:schema:drop
 app/console --env=$ENV doctrine:mongodb:schema:create
 app/console --env=$ENV cache:warm
 chown -R $USER: app/cache/$ENV
+if [ ! -d app/logs ]; then mkdir app/logs; fi;
+if [ ! -f app/logs/$ENV.log ]; then touch app/logs/$ENV.log; fi;
+chown $USER: app/logs/$ENV.log
