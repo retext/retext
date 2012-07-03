733063fbcf4a235ab5b0ee05d8f7c3431293ac3f
Markus Tacker <m@coderbyheart.de>
2012-07-03 11:10:42 +0200

    cache:warmup ist defekt ATM.

--- prototype/server/bin/reset.sh
+++ prototype/server/bin/reset.sh
@@ -13,7 +13,8 @@ app/console --env=$ENV doctrine:mongodb:generate:proxies
 app/console --env=$ENV doctrine:mongodb:generate:repositories RetextApiBundle
 app/console --env=$ENV doctrine:mongodb:schema:drop
 app/console --env=$ENV doctrine:mongodb:schema:create
-app/console --env=$ENV cache:warm
+# Broken, atm
+# app/console --env=$ENV cache:warm
 chown -R $USER: app/cache/$ENV
 if [ ! -d app/logs ]; then mkdir app/logs; fi;
 if [ ! -f app/logs/$ENV.log ]; then touch app/logs/$ENV.log; fi;
